c3f81941abadf082b2c4654755e5bc077540ab6d
pascalllacroix <webiepscf@gmail.com>
2020-06-11 16:37:51 +0200

    Page Contact
    Appel de la page contact depuis le routeur

--- app/routeur.php
+++ app/routeur.php
@@ -4,11 +4,21 @@
   ROUTEUR PRINCIPAL
  */
 
+// PAGE CONTACT
+// PATTERN: ?contact
+// CTRL: -
+// ACTION: -
+  if (isset($_GET['contact'])):
+    $title = "Contact";
+    ob_start();
+      include_once '../app/vues/template/partials/_contact.php';
+    $content = ob_get_clean();
+
 // DETAILS D'UN POST
 // PATTERN: ?postId=x
 // CTRL: postsControleur
 // ACTION: show
-  if (isset($_GET['postId'])):
+  elseif (isset($_GET['postId'])):
     include_once '../app/controleurs/postsControleur.php';
     \App\Controleurs\PostsControleur\showAction($connexion, $_GET['postId']);
 
